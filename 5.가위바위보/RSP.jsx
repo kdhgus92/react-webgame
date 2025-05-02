74b72eb56574d6fa5a0b79cb6bfbcd9e29eb13c1
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-05-02 15:39:49 +0900

    5-7 Custom hook

--- 5.가위바위보/RSP.jsx
+++ 5.가위바위보/RSP.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
+import useInterval from "./useInterval";
 
 const rspCoords = {
   바위: "0",
@@ -20,18 +21,19 @@ const RSP = () => {
   const [result, setResult] = useState("");
   const [imgCoord, setImgCoord] = useState(rspCoords.바위);
   const [score, setScore] = useState(0);
-  const interval = useRef();
+  const [isRunning, setIsRunning] = useState(true);
+  // const interval = useRef();
 
-  useEffect(() => {
-    // componentDidMount, componentDidUpdate 역할 (1대1 대응은 아님)
-    console.log("다시 실행");
-    interval.current = setInterval(changeHand, 100);
-    return () => {
-      // componentWillUnmount 역할
-      console.log("종료");
-      clearInterval(interval.current);
-    };
-  }, [imgCoord]);
+  // useEffect(() => {
+  //   // componentDidMount, componentDidUpdate 역할 (1대1 대응은 아님)
+  //   // console.log("다시 실행");
+  //   interval.current = setInterval(changeHand, 100);
+  //   return () => {
+  //     // componentWillUnmount 역할
+  //     // console.log("종료");
+  //     clearInterval(interval.current);
+  //   };
+  // }, [imgCoord]);
 
   const changeHand = () => {
     if (imgCoord === rspCoords.바위) {
@@ -43,23 +45,28 @@ const RSP = () => {
     }
   };
 
+  useInterval(changeHand, isRunning ? 100 : null);
+
   const onClickBtn = (choice) => () => {
-    clearInterval(interval.current);
-    const myScore = scores[choice];
-    const cpuScore = scores[computerChoice(imgCoord)];
-    const diff = myScore - cpuScore;
-    if (diff === 0) {
-      setResult("비겼습니다!");
-    } else if ([-1, 2].includes(diff)) {
-      setResult("이겼습니다!");
-      setScore((prevScore) => prevScore + 1);
-    } else {
-      setResult("졌습니다!");
-      setScore((prevScore) => prevScore - 1);
+    if (isRunning) {
+      // 멈췄을 때 또 클릭하는 것 막기
+      setIsRunning(false);
+      const myScore = scores[choice];
+      const cpuScore = scores[computerChoice(imgCoord)];
+      const diff = myScore - cpuScore;
+      if (diff === 0) {
+        setResult("비겼습니다!");
+      } else if ([-1, 2].includes(diff)) {
+        setResult("이겼습니다!");
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        setResult("졌습니다!");
+        setScore((prevScore) => prevScore - 1);
+      }
+      setTimeout(() => {
+        setIsRunning(true);
+      }, 1000);
     }
-    setTimeout(() => {
-      interval.current = setInterval(changeHand, 100);
-    }, 1000);
   };
 
   return (
